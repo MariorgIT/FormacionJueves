84e1746ad32c3010ef8ba8f056ac37a87885a583
JoseCNavarro <Jose.Carlos.Navarro.Gonzalez@gds.ey.com>
2024-03-08 10:58:28 +0100

    aa

--- obj/Release/net6.0/CoreBot.AssemblyInfo.cs
+++ obj/Release/net6.0/CoreBot.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreBot")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80fefebcd4c76c16b406c8a49e34e2cf5f789ea8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreBot")]
